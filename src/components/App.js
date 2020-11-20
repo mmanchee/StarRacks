d84b58bd5fa3b8254400d6376b671ce5fe7f5abc
Mike Manchee <mikemanchee@gmail.com>
2020-11-20 09:45:56 -0800

    update App.js in the project

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import CargoControl from "./CargoControl";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div class="contain">
+        <div id="header">
+          <Header />
+        </div>
+        <div id="body">
+          <CargoControl />
+        </div>
+        <div id="footer">
+          <Footer />
+        </div>
+      </div>
+    </React.Fragment>
   );
 }
 
 export default App;
+
