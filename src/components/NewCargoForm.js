c2604907d47f3feb66c3c18f062dcd6771659c78
Mike Manchee <mikemanchee@gmail.com>
2020-11-20 12:40:09 -0800

    update NewCargoForm.js in the project

---
+++ src/components/NewCargoForm.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {v4} from 'uuid'
+import ReusableForm from "./ReusableForm";
+
+function NewCargoForm(props) {
+
+  function handleNewCargoFormSubmission(event) {
+    event.preventDefault();
+    props.onNewCargoCreation({
+      Name: event.target.Name.value, 
+      Description: event.target.Description.value, 
+      Crates: event.target.Crates.value === "" || event.target.Crates.value < 0 ? 0 : event.target.Crates.value,
+      EnergyCredits: event.target.EnergyCredits.value,
+      id:v4()});
+  }
+
+  return (
+    <React.Fragment>
+      <ReusableForm
+        formSubmissionHandler = {handleNewCargoFormSubmission}
+        buttonText = "Add Cargo" />
+    </React.Fragment>
+  )
+};
+
+NewCargoForm.propTypes = {
+  onNewCargoCreation: PropTypes.func
+};
+
+export default NewCargoForm;
