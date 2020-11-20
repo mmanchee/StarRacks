1658601d355a69f05bfafcf052179497768a0fc3
Mike Manchee <mikemanchee@gmail.com>
2020-11-20 14:04:17 -0800

    broke it working on tables

--- src/components/CargoList.js
+++ src/components/CargoList.js
@@ -4,34 +4,40 @@ import PropTypes from 'prop-types';
 
 
 function CargoList(props) {
-  return (
-    <React.Fragment>
-      <table>
-        <tr>
-          <th class="table-name">Name</th>
-          <th class="table-other">Crates</th>
-          <th class="table-other">Energy Credits Total</th>
-        </tr>
-        <tr>
-          <div class="box">
-            <div class="inner-box">
-              {props.Cargos.map((cargo) => 
-              <Cargo
-                whenCargoClicked = {props.onCargoSelection}
-                Name={cargo.Name}
-                Description={cargo.Description}
-                Crates={cargo.Crates}
-                EnergyCredits={cargo.EnergyCredits}
-                id={cargo.id}
-                key={cargo.id}/>
-              )}
-            </div>
+  if(props.Cargo === undefined) {
+    return (
+      <React.Fragment>
+        <p>There are no cargos currently on the ship</p>
+      </React.Fragment>);
+  } else {
+    return (
+      <React.Fragment>
+        <table>
+          <tr>
+            <th class="table-name">Name</th>
+            <th class="table-other">Crates</th>
+            <th class="table-other">Energy Credits Total</th>
+          </tr>
+        </table>
+        <div class="box">
+          <div class="inner-box">
+            
+            {props.Cargos.map((cargo) => 
+            <Cargo
+              whenCargoClicked = {props.onCargoSelection}
+              Name={cargo.Name}
+              Description={cargo.Description}
+              Crates={cargo.Crates}
+              EnergyCredits={cargo.EnergyCredits}
+              id={cargo.id}
+              key={cargo.id}/>
+            )}
           </div>
-        </tr>
-        
-      </table>
-    </React.Fragment>
-  );
+        </div>
+      </React.Fragment>
+    );
+  }
+  
 }
 
 Cargo.propTypes = {
